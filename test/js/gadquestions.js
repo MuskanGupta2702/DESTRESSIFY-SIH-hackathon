4f57b7cb652c6fb00185ae09e52939257a3444d2
manjuchashreya <manjuchashreya2711@gmail.com>
2022-03-15 00:38:04 +0530

    test phq9 ang gad7

---
+++ test/js/gadquestions.js
@@ -0,0 +1,91 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Feeling nervous, anxious or on edge?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every dayNot at all",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Not being able to stop or control worrying?",
+    options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every dayNot at all",
+      ]
+  },
+    {
+    numb: 3,
+    question: "Worrying too much about different things?",
+    options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every dayNot at all",
+      ]
+  },
+    {
+    numb: 4,
+    question: "Trouble relaxing?",
+    options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every dayNot at all",
+      ]
+  },
+    {
+    numb: 5,
+    question: "Being so restless that it is hard to sit still?",
+    options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every dayNot at all",
+      ]
+  },
+  {
+    numb: 6,
+    question: "Becoming easily annoyed or irritable?",
+    options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every dayNot at all",
+      ]
+  },
+  {
+    numb: 7,
+    question: "Feeling afraid as if something awful might happen?",
+    options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every dayNot at all",
+      ]
+  },
+  
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ test/js/questions.js
@@ -0,0 +1,110 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Little interest or pleasure in doing things?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Feeling down, depressed, or hopeless?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Trouble falling or staying asleep, or sleeping too much?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Feeling tired or having little energy?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Poor appetite or overeating?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Feeling bad about yourself - or that you are a failure or have let yourself or your family down?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Trouble concentrating on things, such as reading the newspaper or watching television?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Moving or speaking so slowly that other people could have noticed ? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Thoughts that you would be better off dead, or of hurting yourself in some way?",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half the days",
+      "Nearly every day"
+    ]
+  },
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
